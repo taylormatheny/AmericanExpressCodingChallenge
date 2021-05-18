1f926e88feb7cc2f5ecfa9ee9d9e978de229664c
Taylor Matheny <taylor@mathenys.net>
2021-05-18 13:45:14 -0500

    Step 2: Simple Offer

--- src/main/kotlin/OrderService.kt
+++ src/main/kotlin/OrderService.kt
@@ -1,31 +1,45 @@
 import kotlin.math.round
 
 @OptIn(ExperimentalStdlibApi::class)
+/* currently, this service requires every object in the order to be listed individually. Example: 2 apples does not
+ * work. To improve functionality, you would want to make it so that the user could list the quantity of each item.
+ * This could be done through the command line, or by asking the user for how many they want of each item passed in.
+ */
 
 // The Order Service. Not a true service, since there is no front end. Instead, the order information is stored
 // inside the OrderService class.
 class OrderService(customerOrder: Array<String>) {
-    private var order : MutableList<String> = mutableListOf()
-    private val mapOfCosts = mapOf("apples" to 0.6, "apple" to 0.6, "oranges" to 0.25, "orange" to 0.25)
+    // private var order : MutableList<String> = mutableListOf()
+    // Change the order to a map, so we can track the item and quantity.
+    private val order: MutableMap<String, Int> = mutableMapOf()
+    private val mapOfCosts = mapOf("apple" to 0.6, "orange" to 0.25)
     private var total : Double = 0.0
 
     init {
         // add the order items into the order array after formatting them correctly
         val removePunctuation = Regex("[^a-z0-9]")
         customerOrder.forEach { item ->
-            val updatedItem = removePunctuation.replace(item.lowercase(), "")
+            var updatedItem = removePunctuation.replace(item.lowercase(), "")
+
+            // in case of input such as "apples" instead of "apple"
+            if (mapOfCosts[updatedItem] == null)
+                updatedItem = updatedItem.dropLast(1)
 
             if (mapOfCosts[updatedItem] != null) {
-                order.add(updatedItem)
-                // To ensure correct math on the computer's end and two-decimals of precision
-                total = round((total + mapOfCosts[updatedItem]!!) * 100) / 100
+                if (order[updatedItem] == null) {
+                    order[updatedItem] = 1
+                } else {
+                    order[updatedItem] = order[updatedItem]!! + 1
+                }
             }
         }
+
+        calculateTotal()
     }
 
     // getters
     fun getTotal(): Double { return total }
-    fun getOrder(): MutableList<String> { return order}
+    fun getOrder(): MutableMap<String, Int> { return order}
 
     fun displayTotal() = println("Your total is \$" + "%.2f".format(total))
 
@@ -33,11 +47,28 @@ class OrderService(customerOrder: Array<String>) {
     private fun calculateTotal() {
         total = 0.0
 
-        order.forEach { item ->
-            if (mapOfCosts[item] != null)
-                total += mapOfCosts[item]!!
-            else
-                println("Item $item in your order was not found in the store.")
+        order.forEach { (item, quantity) ->
+            // we round to ensure the computer performs the math operations correctly
+            total = round((total + when(item) {
+                "apple" -> {
+                    val applesToBeChargedFor = calculateDiscountQuantity(quantity, 2, 1)
+                    (applesToBeChargedFor * mapOfCosts[item]!!)
+                }
+                "orange" -> {
+                    val orangesToBeChargedFor = calculateDiscountQuantity(quantity, 3, 2)
+                    (orangesToBeChargedFor * mapOfCosts[item]!!)
+                }
+                else -> {
+                    (quantity * mapOfCosts[item]!!)
+                }
+            }) * 100) / 100
         }
     }
+
+    // this function calculates the number of each item to charge someone for based on the discount. For example, if I
+    // want to put a deal on apples 3 for price of 2, I would use (# of apples, 3, 2) as the parameters
+    private fun calculateDiscountQuantity(quantity: Int, numberToDiscount: Int, discountQuantity: Int): Int {
+        return ((quantity / numberToDiscount) * discountQuantity) + (quantity % numberToDiscount)
+    }
+
 }
--- src/test/kotlin/unitTests.kt
+++ src/test/kotlin/unitTests.kt
@@ -3,39 +3,58 @@ import kotlin.test.assertEquals
 
 class OrderServiceTest {
     // make sure a typical use case displays as expected
-    @Test fun ServiceWorksWithProvidedExample() {
+    @Test fun serviceWorksWithProvidedExample() {
         val order = OrderService(arrayOf("Apple", "Apple", "Orange", "Apple"))
-        assertEquals(order.getTotal(), 2.05)
+        assertEquals(order.getTotal(), 1.45)
     }
 
     // make sure total is $0.00 when no items are given
-    @Test fun ServiceDisplaysZeroWithNoArguments() {
+    @Test fun serviceDisplaysZeroWithNoArguments() {
         val order = OrderService(arrayOf())
         assertEquals(order.getTotal(), 0.00)
     }
 
     // test with capitalized and lowercase words
-    @Test fun ServiceWorksWithCapitalAndLowercase() {
+    @Test fun serviceWorksWithCapitalAndLowercase() {
         val order = OrderService(arrayOf("ORANGE", "apple", "ApPlE", "orANge"))
-        assertEquals(order.getTotal(), 1.70)
+        assertEquals(order.getTotal(), 1.10)
     }
 
     // test with plural and single items
-    @Test fun ServiceWorksWithPluralAndSingleItems() {
+    @Test fun serviceWorksWithPluralAndSingleItems() {
         val order = OrderService(arrayOf("Orange", "Oranges", "Apple", "Apples", "oranges", "apples"))
-        assertEquals(order.getTotal(), 2.55)
+        assertEquals(order.getTotal(), 1.70)
     }
 
     // test with items that we don't have prices for
-    @Test fun ServiceProvidesCorrectPriceWithIncorrectItems() {
+    @Test fun serviceProvidesCorrectPriceWithIncorrectItems() {
         val order = OrderService(arrayOf("Apple", "potatoes", "oranges", "watermelon", "apples", "apples"))
-        assertEquals(order.getTotal(), 2.05)
+        assertEquals(order.getTotal(), 1.45)
     }
 
-    // THESE ARE SOME OTHER IDEAS YOU COULD POTENTIALLY TEST
-    // test with a single item
-    // test with all numbers
-    // test different command line input styles
-    // test with a large number of items
-    // test adding individual items vs. using numbers to specify how many you want of each item
+    // Test with a single item
+    @Test fun orderWithOneItem() {
+        val order = OrderService(arrayOf("apple"))
+        assertEquals(order.getTotal(), 0.60)
+    }
+
+    // Test with invalid number input
+    @Test fun numberInput() {
+        val order = OrderService(arrayOf("123456", "80", "0"))
+        assertEquals(order.getTotal(), 0.00)
+    }
+
+    // Test with commas separating items
+    @Test fun orderWithCommas() {
+        val order = OrderService(arrayOf("apple,", "apple,", "orange,", "apple"))
+        assertEquals(order.getTotal(), 1.45)
+    }
+
+    // Test with a large input
+    @Test fun largeOrder() {
+        val order = OrderService(arrayOf("apple", "orange", "orange", "apple", "apple", "apple", "orange", "orange",
+        "orange", "orange", "apple", "apple", "apple", "apple", "apple", "orange", "orange", "orange", "orange",
+        "orange", "orange", "apple", "apple", "apple", "apple", "apple", "apple", "apple", "orange", "orange", "apple"))
+        assertEquals(order.getTotal(), 7.90)
+    }
 }
